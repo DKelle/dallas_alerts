aaeb634a71df119f276f45caeca687910b2b5133
Ubuntu <ubuntu@ip-172-31-23-62.us-east-2.compute.internal>
2018-02-12 01:30:15 +0000

    Changing the logic that determines when to tweet

--- detections.py
+++ detections.py
@@ -7,12 +7,18 @@ from datetime import datetime
 import logging
 
 logging.basicConfig(filename='alerts.log',level=logging.INFO)
-def ecoin_value(fsym, low, high):
+def ecoin_value(fsym, interval):
     info("Initiating worker for {}".format(fsym))
     # Here's an API that returns the value of different crypto currency
     BASE_URL = "https://min-api.cryptocompare.com/data/price?fsym={}&tsyms=USD".format(fsym)
 
-    low = True
+    interval_count = 0
+
+    # Keep track of which direction this coin is trending
+    UP = 1
+    DOWN = 2
+    direction = UP 
+    last_direction = UP
     while True:
         r = requests.get(BASE_URL)
 
@@ -20,23 +26,34 @@ def ecoin_value(fsym, low, high):
             value = json.loads(r.content)['USD']
             info("value for {} is {}".format(fsym, value))
 
-            # We want to tweet as soon as we cross the high threshold
-            # And then not again until we fall under the low threshold
-            if low and value > high:
-                # We just went from being below the low value to above the high value
-                # eg, ETH going from 750 -> 900
-                low = False
-                msg = "{} has risen to {}".format(fsym, value)
-                info("TWEETING: {}".format(msg))
-                tweet(msg)
+            # Count how many 'intervals' the value currently is
+            # eg, value of 100, and interval of 50 has count 2
+            next_interval_count = int(value / interval)
+            info("interval for {} is {}".format(fsym, next_interval_count))
+            if next_interval_count > interval_count:
+                direction = UP
 
-            elif not low and value < low:
-                # We just went from being above the high value to below the low value 
-                # eg, ETH going from 900 -> 750
-                low = True
-                msg = "{} has fallen to {}".format(fsym, value)
-                info("TWEETING: {}".format(msg))
-                tweet(msg)
+                # Has this coin risen twice in a row?
+                if last_direction == UP:
+                    msg = "{} has risen to {}".format(fsym, value)
+                    info("TWEETING: {}".format(msg))
+                    tweet(msg)
+
+                last_direction = UP
+
+
+            elif next_interval_count < interval_count:
+                direction = DOWN
+
+                # Has this coin dropped twice in a row?
+                if last_direction == DOWN:
+                    msg = "{} has fallen to {}".format(fsym, value)
+                    info("TWEETING: {}".format(msg))
+                    tweet(msg)
+
+                last_direction = DOWN
+
+            interval_count = next_interval_count
 
         except Exception as e:
             exc("Thread {} encountered a key error".format(fsym))
@@ -47,12 +64,12 @@ def ecoin_value(fsym, low, high):
     
 def main():
     logging.info("Starting worker threads!\n\n")
-    workers = {"ETH": [750, 900],
-            "BTC": [7750, 10000],
-            "LTC": [120, 160]}
+    workers = {"ETH": 35,
+            "BTC": 500,
+            "LTC": 20}
 
     for worker in workers:
-        Thread(target=ecoin_value, args=(worker, workers[worker][0], workers[worker][1])).start()
+        Thread(target=ecoin_value, args=(worker, workers[worker])).start()
 
 def info(msg):
     date = datetime.now()
